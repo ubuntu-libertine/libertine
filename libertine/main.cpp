689bc3881c4e44a66538e3b4b668512fc54c484b
Stephen M. Webb <stephen.webb@canonical.com>
2015-05-12 11:10:22 -0400

    initial project creation

---
+++ libertine/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file main.cpp
+ * @brief Libertine app mainline driver.
+ */
+/*
+ * Copyright 2015 Canonical Ltd
+ *
+ * Libertine is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 3, as published by the
+ * Free Software Foundation.
+ *
+ * Libertine is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "libertine/config.h"
+
+int
+main(int, char*[])
+{
+}
