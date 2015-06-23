2d1efc6c77195190947c408e625e55b575071ea3
Chris Townsend <christopher.townsend@canonical.com>
2015-06-23 12:53:23 -0400

    Add file copyright headers
    Add copyright headers to the new files.

--- libertine/ContainerManager.h
+++ libertine/ContainerManager.h
@@ -1,4 +1,21 @@
-
+/**
+ * @file ContainerManager.h
+ * @brief Threaded Libertine container manager
+ */
+/*
+ * Copyright 2015 Canonical Ltd
+ *
+ * Libertine is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 3, as published by the
+ * Free Software Foundation.
+ *
+ * Libertine is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef CONTAINER_CONTAINERMANAGER_H_
 #define CONTAINER_CONTAINERMANAGER_H_
 
