c07da1cfe1303e0403e3058de2c4617f2400da71
Chris Townsend <christopher.townsend@canonical.com>
2015-07-16 09:46:45 -0400

    Add stdout/stderr redirector in Python for capturing output
    Add a stdout/stderr redirector in the Python code for capturing output from lxc-attach calls.  This is needed to pass status, error messages, and data back to the Libertine manager.

--- libertine/LibertineContainerTools.py
+++ libertine/LibertineContainerTools.py
@@ -5,8 +5,69 @@ import subprocess
 import os
 import lsb_release
 import crypt
+import ctypes
+import tempfile
+
+from contextlib import contextmanager
 
 home_path = os.environ['HOME']
+
+libc = ctypes.CDLL(None)
+c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
+
+@contextmanager
+def output_redirector(stream):
+    # The original fd stdout points to. Usually 1 on POSIX systems.
+    original_stdout_fd = sys.stdout.fileno()
+    original_stderr_fd = sys.stderr.fileno()
+
+    def _redirect_output(to_fd):
+        """Redirect stdout/stderr to the given file descriptor."""
+        # Flush the C-level buffer stdout
+        libc.fflush(c_stdout)
+        # Flush and close sys.stdout/sys.stderr - also closes the file descriptor (fd)
+        sys.stdout.close()
+        sys.stderr.close()
+        # Make original_stdout_fd point to the same file as to_fd
+        os.dup2(to_fd, original_stdout_fd)
+        # Create a new sys.stdout that points to the redirected fd
+        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
+
+        # Make original_stderr_fd point to sys.stdout fd
+        os.dup2(sys.stdout.fileno(), original_stderr_fd)
+        # Create a new sys.stderr that points to the redirected fd
+        sys.stderr = io.TextIOWrapper(os.fdopen(original_stderr_fd, 'wb'))
+
+    def _restore_output(stdout_fd, stderr_fd):
+        libc.fflush(c_stdout)
+
+        sys.stdout.close()
+        sys.stderr.close()
+
+        os.dup2(stdout_fd, original_stdout_fd)
+        os.dup2(stderr_fd, original_stderr_fd)
+
+        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
+        sys.stderr = io.TextIOWrapper(os.fdopen(original_stderr_fd, 'wb'))
+
+    # Save a copy of the original stdout fd in saved_stdout_fd
+    saved_stdout_fd = os.dup(original_stdout_fd)
+    saved_stderr_fd = os.dup(original_stderr_fd)
+    try:
+        # Create a temporary file and redirect stdout to it
+        tfile = tempfile.TemporaryFile(mode='w+b')
+        _redirect_output(tfile.fileno())
+        # Yield to caller, then redirect stdout back to the saved fd
+        yield
+        _restore_output(saved_stdout_fd, saved_stderr_fd)
+        # Copy contents of temporary file to the given stream
+        tfile.flush()
+        tfile.seek(0, io.SEEK_SET)
+        stream.write(tfile.read())
+    finally:
+        tfile.close()
+        os.close(saved_stdout_fd)
+        os.close(saved_stderr_fd)
 
 def check_lxc_net_entry(entry):
     lxc_net_file = open('/etc/lxc/lxc-usernet')
