e0784679aa5a61c3ec7747bc09dae9041bef7703
Chris Townsend <christopher.townsend@canonical.com>
2015-09-03 15:41:44 +0000

    Merge in changes from lp:libertine/trunk.

--- libertine/PasswordHelper.h
+++ libertine/PasswordHelper.h
@@ -1,3 +1,21 @@
+/**
+ * @file PasswordHelper.h
+ * @brief Helper class for managing password entry
+ */
+/*
+ * Copyright 2015 Canonical Ltd
+ *
+ * Libertine is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, version 3, as published by the
+ * Free Software Foundation.
+ *
+ * Libertine is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef LIBERTINE_PASSWORD_HELPER_H_
 #define LIBERTINE_PASSWORD_HELPER_H_
 
